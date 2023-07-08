9502bfbb99fca397bbb5b01192a3c99b01d97fea
Postmodern <postmodern.mod3@gmail.com>
2023-07-08 01:45:52 -0700

    Added `Ronin::DB::Note` (closes #33).

---
+++ spec/note_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'ronin/db/note'
+
+describe Ronin::DB::Note do
+  describe "validations" do
+    it "must require a body" do
+      note = described_class.new
+
+      expect(note).to_not be_valid
+      expect(note.errors[:body]).to eq(
+        ["can't be blank"]
+      )
+    end
+
+    it "must have at least one belongs_to association set" do
+      note = described_class.new(body: 'Test')
+
+      expect(note).to_not be_valid
+      expect(note.errors[:base]).to eq(
+        ['note must be associated with a MACAddress, IPAddress, HostName, Port, Service, OpenPort, Cert, URL, UserName, EmailAddress, Password, Credential, Advisory, or Organization']
+      )
+
+      note = described_class.new(
+        body:       'Test',
+        ip_address: Ronin::DB::IPAddress.new(
+          address: '192.168.1.1'
+        )
+      )
+      expect(note).to be_valid
+    end
+  end
+end
