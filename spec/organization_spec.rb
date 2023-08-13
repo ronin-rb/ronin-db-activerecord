bda239ccd5a2cc38ef211732a443f60d225b3c2e
Postmodern <postmodern.mod3@gmail.com>
2023-08-12 23:12:47 -0700

    Make `Organization` importable.
    * Added `Organization.lookup` and `Organization.import`.

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -7,4 +7,33 @@ describe Ronin::DB::Organization do
   end
 
   let(:name) { 'ACME, Corp.' }
+
+  describe ".lookup" do
+    before do
+      described_class.create(name: 'Foo, Inc.')
+      described_class.create(name: name)
+      described_class.create(name: 'Bar, Inc.')
+    end
+
+    it "must query the #{described_class} with the matching name" do
+      organization = described_class.lookup(name)
+
+      expect(organization).to be_kind_of(described_class)
+      expect(organization.name).to eq(name)
+    end
+
+    after { described_class.destroy_all }
+  end
+
+  describe ".import" do
+    subject { described_class.import(name) }
+
+    it "must import the #{described_class} and set #name" do
+      expect(subject).to be_kind_of(described_class)
+      expect(subject.id).to_not be(nil)
+      expect(subject.name).to eq(name)
+    end
+
+    after { described_class.destroy_all }
+  end
 end
