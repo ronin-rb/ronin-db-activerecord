1f3c6f5e0bf0a8e2a7a2ae5d9019aafa7825dbd7
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-30 18:26:30 +0100

    Add an optional `source_ip` column to the `HTTPRequest` model (#107)

---
+++ db/migrate/0061_add_source_ip_column_to_http_requests_table.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+#
+# ronin-db-activerecord - ActiveRecord backend for the Ronin Database.
+#
+# Copyright (c) 2022-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-db-activerecord.
+#
+# ronin-db-activerecord is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-db-activerecord is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-db-activerecord.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+# Adds the `source_ip` column to the `ronin_http_requests` table.
+#
+class AddSourceIpColumnToHttpRequestsTable < ActiveRecord::Migration[7.0]
+  def change
+    add_column :ronin_http_requests, :source_ip, :string, length: 39, null: true
+  end
+end
