a448e21bf3c81a9d65264a6384de77160e165c10
Postmodern <postmodern.mod3@gmail.com>
2023-12-21 14:06:51 -0800

    Added `OrganizationMember#to_s`.

--- spec/organization_member_spec.rb
+++ spec/organization_member_spec.rb
@@ -5,4 +5,32 @@ describe Ronin::DB::OrganizationMember do
   it "must use the 'ronin_organization_members' table" do
     expect(described_class.table_name).to eq('ronin_organization_members')
   end
+
+  let(:first_name) { 'John' }
+  let(:last_name)  { 'Smith' }
+  let(:full_name)  { "#{first_name} #{last_name}" }
+
+  let(:person) do
+    Ronin::DB::Person.new(
+      full_name:  full_name,
+      first_name: first_name,
+      last_name:  last_name
+    )
+  end
+
+  let(:organization_name) { 'ACME, Corp.' }
+  let(:organization) { Ronin::DB::Organization.new(name: organization_name) }
+
+  subject do
+    described_class.new(
+      organization: organization,
+      person:       person
+    )
+  end
+
+  describe "#to_s" do
+    it "must return the #person's full name" do
+      expect(subject.to_s).to eq(person.to_s)
+    end
+  end
 end
