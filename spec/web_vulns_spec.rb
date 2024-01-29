0f5cfbd5b6d7a6b81e841d96678c036342656ed1
Postmodern <postmodern.mod3@gmail.com>
2024-01-29 06:42:53 -0800

    Added `WebVuln.with_query_param` (issue #114).

--- spec/web_vulns_spec.rb
+++ spec/web_vulns_spec.rb
@@ -56,6 +56,57 @@ describe Ronin::DB::WebVuln do
     end
   end
 
+  describe ".with_query_param" do
+    subject { described_class }
+
+    let(:query_param) { 'foo' }
+
+    before do
+      url = Ronin::DB::URL.import('https://example.com/page.php?id=1&foo=2&bar=3&baz=4')
+
+      Ronin::DB::WebVuln.create(
+        type:           :sqli,
+        request_method: :get,
+        query_param:    'id',
+        url:            url
+      )
+
+      Ronin::DB::WebVuln.create(
+        type:           :lfi,
+        request_method: :get,
+        query_param:    query_param,
+        url:            url
+      )
+
+      Ronin::DB::WebVuln.create(
+        type:           :rfi,
+        request_method: :get,
+        query_param:    query_param,
+        url:            url
+      )
+    end
+
+    it "must query all #{described_class}s with the matching #query_param" do
+      web_vulns = subject.with_query_param(query_param)
+
+      expect(web_vulns.length).to eq(2)
+
+      expect(web_vulns[0].type).to eq('lfi')
+      expect(web_vulns[0].query_param).to eq(query_param)
+
+      expect(web_vulns[1].type).to eq('rfi')
+      expect(web_vulns[1].query_param).to eq(query_param)
+    end
+
+    after do
+      Ronin::DB::WebVuln.destroy_all
+      Ronin::DB::URL.destroy_all
+      Ronin::DB::URLQueryParamName.destroy_all
+      Ronin::DB::URLScheme.destroy_all
+      Ronin::DB::HostName.destroy_all
+    end
+  end
+
   let(:url_scheme)    { Ronin::DB::URLScheme.find_or_initialize_by(name: 'https') }
   let(:url_host_name) { Ronin::DB::HostName.find_or_initialize_by(name: 'www.example.com') }
   let(:url_port)      { Ronin::DB::Port.find_or_initialize_by(protocol: :tcp, number: 8080) }
