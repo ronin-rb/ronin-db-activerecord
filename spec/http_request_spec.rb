aa2b4ed8149e7e0bebaf5a454102d3d96f3dca2f
Postmodern <postmodern.mod3@gmail.com>
2025-05-03 18:14:36 -0700

    Added whitespace to the `HTTPRequest` validation specs for readability.

--- spec/http_request_spec.rb
+++ spec/http_request_spec.rb
@@ -85,46 +85,51 @@ describe Ronin::DB::HTTPRequest do
       let(:ipv6_address) { '2606:2800:220:1:248:1893:25c8:1946' }
 
       it "must not require an address" do
-        http_request = described_class.new
-        expect(http_request).to_not be_valid
+        request = described_class.new
 
-        http_request = described_class.new(
+        expect(request).to_not be_valid
+
+        request = described_class.new(
           version:        version,
           request_method: request_method,
           path:           path
         )
-        expect(http_request).to be_valid
+
+        expect(request).to be_valid
       end
 
       it "must accept IPv4 addresses" do
-        http_request = described_class.new(
+        request = described_class.new(
           version:        version,
           request_method: request_method,
           path:           path,
           source_ip:      ipv4_address
         )
-        expect(http_request).to be_valid
+
+        expect(request).to be_valid
       end
 
       it "must accept IPv6 addresses" do
-        http_request = described_class.new(
+        request = described_class.new(
           version:        version,
           request_method: request_method,
           path:           path,
           source_ip:      ipv6_address
         )
-        expect(http_request).to be_valid
+
+        expect(request).to be_valid
       end
 
       it "must not accept non-IP addresses" do
-        http_request = described_class.new(
+        request = described_class.new(
           version:        version,
           request_method: request_method,
           path:           path,
           source_ip:      '0'
         )
-        expect(http_request).to_not be_valid
-        expect(http_request.errors[:source_ip]).to eq(
+
+        expect(request).to_not be_valid
+        expect(request.errors[:source_ip]).to eq(
           ["Must be a valid IP address"]
         )
       end
